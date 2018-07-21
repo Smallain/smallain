fd987e615ad4be795be9330605a8e4e7c191bb00
Wu Yuhang <smallainwuyuhang@163.com>
2018-07-21 20:32:30 +0800

    first commit

--- Capfile
+++ Capfile
@@ -31,7 +31,7 @@ require "capistrano/rbenv"
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 require "capistrano/bundler"
-require "capistrano/rails"
+# require "capistrano/rails"
 require "capistrano/passenger"
 
 # require "capistrano/rvm"
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "smallain"
+set :repo_url, "https://github.com/Smallain/smallain.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/treenewbee/smallain"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -21,7 +21,7 @@ set :repo_url, "git@example.com:me/my_repo.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
